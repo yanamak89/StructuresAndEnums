75bff6bfe134c21b9b70e2405858a4a9dd873ad6
Yana Makogon <yana.mak89@gmail.com>
2024-06-03 11:28:31 +0200

    Initial commit

---
+++ Task2/ConsoleColorEnum.cs
@@ -0,0 +1,12 @@
+namespace Task2;
+
+// Перелік доступних кольорів
+enum EnumColors
+{
+    black = ConsoleColor.Black, 
+    blue = ConsoleColor.Blue, 
+    red = ConsoleColor.Red, 
+    white = ConsoleColor.White,
+    green = ConsoleColor.Green,
+    yellow = ConsoleColor.Yellow
+}
---
+++ Task3/Post.cs
@@ -0,0 +1,10 @@
+namespace Task3;
+
+// Перелік посад співробітників і відповідні години роботи
+public enum PositionEnum
+{
+    Manager = 160, // 160 годин на місяць
+    Developer = 150, // 150 годин на місяць
+    QAEngineer = 140, // 140 годин на місяць
+    Designer = 130
+}
